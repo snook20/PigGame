8dd09d7b404a76a80ce8021c372f7a8877d58e6e
snook20 <snook20@up.edu>
2018-03-01 16:57:23 -0800

    this sucks

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,19 @@
+package edu.up.cs301.pig;
+
+        import edu.up.cs301.game.infoMsg.GameState;
+
+/**
+ * Created by snook20 on 3/1/2018.
+ */
+
+public class PigGameState extends GameState{
+    int playerID;
+    int playerOneScore;
+    int playerTwoScore;
+    int runningTotal;
+    int currentDieValue;
+
+    public PigGameState(){
+
+    }
+}
